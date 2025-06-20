cb4f20f38a94a6b026730d32be6ff3532964b407
Paulo Cesar Santos Filho <111451739+CesarSantos2p0@users.noreply.github.com>
2025-06-20 15:49:26 -0300

    Initial commit

---
+++ src/main/java/tech/buildrun/API_de_tarefas/ApiDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.API_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeTarefasApplication.class, args);
+	}
+
+}
